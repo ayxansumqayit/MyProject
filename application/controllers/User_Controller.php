62f37a49f0ca62ef2812dbebf85a1c7d695988ec
Ayxan Mustafayev <118119951+ayxansumqayit@users.noreply.github.com>
2023-04-24 16:07:54 +0400

    My First Php Project

---
+++ application/controllers/User_Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+class User_Controller extends CI_Controller{
+
+    public function index(){
+        echo "Men index metoduyam";
+    }
+
+    public function about(){
+        echo "Men about metoduyam";
+    }
+}
